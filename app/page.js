3451ed01b401cda5610ece5755c1f74ab7edab0e
mayzinei <mayzinei1998@gmail.com>
2023-12-28 19:02:46 +0630

    adding nav bar

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
+"use client";
+import { TfiStatsUp } from "react-icons/tfi";
+
 export default function Home() {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<header className="container max-w-screen-md mx-auto flex items-center justify-between">
+			<div>
+				<h1 className="text-3xl font-bold bg-red-600 text-white text-center hover:tracking-widest duration-300">
+					onion
+				</h1>
+				<p className="text-xs">track your expenses</p>
+			</div>
+			<div className="flex items-center gap-3 md:gap-6">
+				<TfiStatsUp className="text-2xl" />
+				<div className="w-[40px] h-[40px] rounded-full border border-red-600">
+					<img
+						className="w-full h-full object-cover rounded-full"
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREO17hg6KvLlweeZWN0LCEdi-OXM9qGpbQ9w&usqp=CAU"
+						alt="user profile"
+					/>
+				</div>
+			</div>
+		</header>
 	);
 }
