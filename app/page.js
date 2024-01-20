6b8806091d160c577eed2deafaeaefd4f02686df
mayzinei <mze1001web@gmail.com>
2024-01-20 20:16:25 +0630

    add incomeModelBox

--- app/page.js
+++ app/page.js
@@ -1,125 +1,77 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { currencyFormatter } from "@/lib/utils";
 import ExpenseItems from "@/components/expenseItems";
 import Stats from "@/components/stats";
 import ModelBox from "@/components/modelBox";
+import IncomeModelBox from "@/components/incomeModelBox";
 const DammyData = [
-	{ color: "#EE7214", title: "Clothing", amount: "6000" },
-	{ color: "#527853", title: "Skin Care", amount: "40000" },
-	{ color: "#F9E8D9", title: "Eating", amount: "75000" },
-	{ color: "#F7B787", title: "Clothing", amount: "6000" },
-	{ color: "#FFC5C5", title: "Clothing", amount: "6000" },
+  { color: "#EE7214", title: "Clothing", amount: "6000" },
+  { color: "#527853", title: "Skin Care", amount: "40000" },
+  { color: "#F9E8D9", title: "Eating", amount: "75000" },
+  { color: "#F7B787", title: "Clothing", amount: "6000" },
+  { color: "#FFC5C5", title: "Clothing", amount: "6000" },
 ];
 
 export default function Home() {
-	const [incomeModelOpen, setIncomeModelOpen] = useState(true);
-	const descriptionRef = useRef();
-	const amountRef = useRef();
-	const incomeHandler = (e) => {
-		e.preventDefault();
+  const [modelOpen, setModelOpen] = useState(false);
 
-		const newIncomes = {
-			description: descriptionRef.current.value,
-			amount: amountRef.current.value,
-			createdAt: new Date(),
-		};
+  return (
+    <>
+      {/* --------Income Model Box------- */}
+      <ModelBox show={modelOpen} onClose={setModelOpen}>
+        <IncomeModelBox />
+      </ModelBox>
 
-		console.log(newIncomes);
-	};
-	return (
-		<>
-			{/* Income Model Box */}
-			<ModelBox show={incomeModelOpen} onClose={setIncomeModelOpen}>
-				<form onSubmit={incomeHandler} className="space-y-4">
-					<div className="flex flex-col gap-2">
-						<label htmlFor="amount" className="text-sm">
-							Enter Income Description
-						</label>
-						<input
-							className="inputBox"
-							name="description"
-							ref={descriptionRef}
-							type="text"
-							required
-						/>
-					</div>
-					<div className="flex flex-col gap-2">
-						<label htmlFor="amount" className="text-sm">
-							Enter Income Amount
-						</label>
-						<input
-							className="inputBox"
-							name="amount"
-							type="number"
-							ref={amountRef}
-							min={0.1}
-							step={0.1}
-							required
-						/>
-					</div>
-					<div className="flex items-center justify-end gap-2">
-						<button type="reset" className="expenseBtn bg-accentColor/20">
-							Cancel
-						</button>
-						<button type="submit" className="incomeBtn bg-greenColor/20">
-							Add
-						</button>
-					</div>
-				</form>
-			</ModelBox>
+      <main className="container max-w-screen-md mx-auto space-y-4 ">
+        {/* Balance Section */}
+        <section className="flex flex-col md:flex-row items-center justify-between gap-4">
+          <div>
+            <p className="text-xs text-grayColor text-center md:text-left">
+              Your Balance
+            </p>
+            <h2 className="text-xl font-bold ">{currencyFormatter(300000)}</h2>
+          </div>
+          <div className="flex items-center gap-2">
+            <button className="expenseBtn">+ Expenses</button>
+            <button
+              onClick={() => setModelOpen(!modelOpen)}
+              className="incomeBtn"
+            >
+              + Incomes
+            </button>
+          </div>
+        </section>
 
-			<main className="container max-w-screen-md mx-auto space-y-4 ">
-				{/* Balance Section */}
-				<section className="flex flex-col md:flex-row items-center justify-between gap-4">
-					<div>
-						<p className="text-xs text-grayColor text-center md:text-left">
-							Your Balance
-						</p>
-						<h2 className="text-xl font-bold ">
-							{currencyFormatter(300000)}
-						</h2>
-					</div>
-					<div className="flex items-center gap-2">
-						<button className="expenseBtn">+ Expenses</button>
-						<button
-							onClick={() => setIncomeModelOpen(true)}
-							className="incomeBtn"
-						>
-							+ Incomes
-						</button>
-					</div>
-				</section>
+        {/* Expenses Section */}
+        <section className="container space-y-6">
+          <div className="flex items-center justify-between">
+            <button className="lgButton">Expenses</button>
+            <button className="lgButton">Incomes</button>
+          </div>
+          <div className="space-y-2">
+            {DammyData.map((data, index) => {
+              return (
+                <ExpenseItems
+                  key={index}
+                  color={data.color}
+                  title={data.title}
+                  amount={data.amount}
+                />
+              );
+            })}
+          </div>
+        </section>
 
-				{/* Expenses Section */}
-				<section className="container space-y-6">
-					<div className="flex items-center justify-between">
-						<button className="lgButton">Expenses</button>
-						<button className="lgButton">Incomes</button>
-					</div>
-					<div className="space-y-2">
-						{DammyData.map((data, index) => {
-							return (
-								<ExpenseItems
-									key={index}
-									color={data.color}
-									title={data.title}
-									amount={data.amount}
-								/>
-							);
-						})}
-					</div>
-				</section>
-
-				{/* Stats Section */}
-				<section className="container">
-					<Stats
-						color={DammyData.map((e) => e.color)}
-						title={DammyData.map((e) => e.title)}
-						amount={DammyData.map((e) => e.amount)}
-					/>
-				</section>
-			</main>
-		</>
-	);
+        {/* Stats Section */}
+        <section className="container">
+          <Stats
+            color={DammyData.map((e) => e.color)}
+            title={DammyData.map((e) => e.title)}
+            amount={DammyData.map((e) => e.amount)}
+          />
+        </section>
+      </main>
+    </>
+  );
 }
--- components/modelBox.js
+++ components/modelBox.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 export default function ModelBox({ show, onClose, children }) {
-	return (
-		<div
-			style={{
-				transform: show ? "translateY(0%)" : "translateY(-200%)",
-			}}
-			className="absolute container top-14 left-0 transition-all duration-200"
-		>
-			<div className="container max-w-2xl mx-auto h-[80vh] bg-grayColor/20 backdrop-blur-md rounded-lg">
-				<div className="flex justify-end">
-					<button
-						onClick={() => onClose(false)}
-						className="px-3.5 py-2 bg-bgColor/50 rounded-full font-bold"
-					>
-						X
-					</button>
-				</div>
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        transform: show ? "translateY(0%)" : "translateY(-200%)",
+      }}
+      className="absolute container top-14 left-0 transition-all duration-200"
+    >
+      <div className="container max-w-2xl mx-auto bg-grayColor/20 backdrop-blur-md rounded-lg">
+        <div className="flex justify-end">
+          <button
+            onClick={() => onClose(!show)}
+            className="px-3.5 py-2 bg-bgColor/50 rounded-full font-bold"
+          >
+            X
+          </button>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
 }
